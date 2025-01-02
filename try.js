2e8e679695fc1b2ec99c6b854af33e0c01bf1248
sanketpatelcoding <androsanket@gmail.com>
2025-01-02 09:05:53 +0530

    all1

--- try.js
+++ try.js
@@ -1,6 +1,6 @@
 // Function to fetch data from the JSON file
 const url = 'https://raw.githubusercontent.com/sanketpatelcoding/sampleWeek/master/data.json';
-
+const url1 ='https://goweather.herokuapp.com/weather/Nj';
 fetch(url) // Adjust the URL if necessary
     .then(response => {
         if (!response.ok) {
@@ -15,3 +15,12 @@ fetch(url) // Adjust the URL if necessary
     .catch(error => {
         console.error('There was a problem with the fetch operation:', error);
     });
+
+    fetch(url1).then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+    }).then(data1=>{
+        console.log(data1.forecast)
+    })
